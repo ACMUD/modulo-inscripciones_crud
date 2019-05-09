5a70ca8847e27e53e236bdb029dd527332755da7
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-05-08 23:33:40 -0500

    adicion de controladores y modelos faltantes

--- models/horario_persona.go
+++ models/horario_persona.go
@@ -1,6 +1,151 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
 type HorarioPersona struct {
-	HorarioId *Horario `orm:"column(horario_id);rel(fk)"`
-	PersonaId *Persona `orm:"column(persona_id);rel(fk)"`
+	Id      int      `orm:"column(id);pk"`
+	Horario *Horario `orm:"column(horario);rel(fk)"`
+	Persona *Persona `orm:"column(persona);rel(fk)"`
+}
+
+func (t *HorarioPersona) TableName() string {
+	return "horario_persona"
+}
+
+func init() {
+	orm.RegisterModel(new(HorarioPersona))
+}
+
+// AddHorarioPersona insert a new HorarioPersona into database and returns
+// last inserted Id on success.
+func AddHorarioPersona(m *HorarioPersona) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetHorarioPersonaById retrieves HorarioPersona by Id. Returns error if
+// Id doesn't exist
+func GetHorarioPersonaById(id int) (v *HorarioPersona, err error) {
+	o := orm.NewOrm()
+	v = &HorarioPersona{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+// GetAllHorarioPersona retrieves all HorarioPersona matches certain condition. Returns empty list if
+// no records exist
+func GetAllHorarioPersona(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(HorarioPersona))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []HorarioPersona
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdateHorarioPersona updates HorarioPersona by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateHorarioPersonaById(m *HorarioPersona) (err error) {
+	o := orm.NewOrm()
+	v := HorarioPersona{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeleteHorarioPersona deletes HorarioPersona by Id and returns error if
+// the record to be deleted doesn't exist
+func DeleteHorarioPersona(id int) (err error) {
+	o := orm.NewOrm()
+	v := HorarioPersona{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&HorarioPersona{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
 }
